f044fee00be248eba891f697c5721e4a6fd49fd6
Kai Wohlfahrt <kjw53@cam.ac.uk>
2017-07-28 12:49:20 +0100

    Allow unstacking along an axis

---
+++ tests/test_unstack.py
@@ -0,0 +1,42 @@
+import pytest
+import numpy as np
+
+from tifffile import TiffWriter, TiffFile
+
+from tiffutil.unstack import *
+
+from click.testing import CliRunner
+
+@pytest.fixture
+def runner():
+    return CliRunner()
+
+def test_unstack(tmpdir, runner):
+    data = np.random.randint(0, 200, size=(10, 32, 32), dtype='uint8')
+    tifpath = tmpdir.join("test.tif")
+    with TiffWriter(str(tifpath)) as f:
+        f.save(data)
+
+    outputs = list(map(str, map(tmpdir.join, map("out{}.tif".format, range(2)))))
+    result = runner.invoke(unstack, [str(tifpath)] + outputs)
+    assert result.exit_code == 0
+
+    expecteds = np.rollaxis(data.reshape(5, 2, 32, 32), 1)
+    for output, expected in zip(map(TiffFile, outputs), expecteds):
+        with output as f:
+            np.testing.assert_equal(f.asarray(), expected)
+
+def test_unstack_axis(tmpdir, runner):
+    data = np.random.randint(0, 200, size=(4, 10, 32, 32), dtype='uint8')
+    tifpath = tmpdir.join("test.tif")
+    with TiffWriter(str(tifpath)) as f:
+        f.save(data)
+
+    outputs = list(map(str, map(tmpdir.join, map("out{}.tif".format, range(2)))))
+    result = runner.invoke(unstack, ["--axis", "1", str(tifpath)] + outputs)
+    assert result.exit_code == 0
+
+    expecteds = np.rollaxis(data.reshape(20, 2, 32, 32), 1)
+    for output, expected in zip(map(TiffFile, map(str, outputs)), expecteds):
+        with output as f:
+            np.testing.assert_equal(f.asarray(), expected)
--- tiffutil/unstack.py
+++ tiffutil/unstack.py
@@ -1,4 +1,5 @@
 import click
+import numpy as np
 
 from contextlib import ExitStack
 from itertools import cycle
@@ -9,16 +10,18 @@ from tifffile import TiffWriter
 
 from .util import SingleTiffFile
 
-def unstack(data, nstacks):
-    slices = (slice(i, None, nstacks) for i in range(nstacks))
-    return (data[s] for s in slices)
-
 @click.command()
 @click.argument("image", type=SingleTiffFile)
-@click.argument("outputs", type=Path)
-def unstack(image, outputs):
+@click.argument("outputs", type=TiffWriter, nargs=-1)
+@click.option("--axis", type=int, default=0)
+@click.option("--series", type=int, default=0)
+def unstack(image, outputs, axis=0, series=0):
     with image as tif, ExitStack() as output_stack:
-        outfiles = [output_stack.enter_context(TiffWriter(str(path)))
-                    for path in outputs]
-        for outfile, page in zip(cycle(outfiles), iter(tif.pages)):
+        outfiles = [output_stack.enter_context(output) for output in outputs]
+        series = tif.series[series]
+
+        pages = np.asarray(series.pages).reshape(series.shape[:-2]).T
+        pages = np.rollaxis(pages, axis)
+
+        for outfile, page in zip(cycle(outfiles), pages.ravel()):
             outfile.save(page.asarray())
